2918185258a5aac5afedebf913df1f76ddefc26d
Prasannajit Acharya - Kanhu <prasannajit.acharya.kanhu@gmail.com>
2014-07-06 18:22:29 +0530

    Added downloadURL and updateURL to Text View Mode

--- Text View Mode/wikiHow Text View Mode.meta.js
+++ Text View Mode/wikiHow Text View Mode.meta.js
@@ -2,6 +2,8 @@
 // @name wikiHow Text View Mode
 // @namespace https://github.com/paKanhu/wikiHow-userscripts
 // @description Changes wikiHow Categories view mode to text
+// @downloadURL https://github.com/paKanhu/wikiHow-userscripts/raw/master/Text%20View%20Mode/wikiHow%20Text%20View%20Mode.user.js
+// @updateURL https://github.com/paKanhu/wikiHow-userscripts/raw/master/Text%20View%20Mode/wikiHow%20Text%20View%20Mode.meta.js
 // @include http://www.wikihow.com/Category:*
 // @include http://hi.wikihow.com/%E0%A4%B6%E0%A5%8D%E0%A4%B0%E0%A5%87%E0%A4%A3%E0%A5%80:*
 // @include http://es.wikihow.com/Categor%C3%ADa:*
